ce4e0f9ea890a2b2f3ec9d7fc987b4bfbcc26924
qianqian <308456341@qq.com>
2025-01-17 18:18:01 +0800

    a

--- ctrl.sh
+++ ctrl.sh
@@ -1,8 +1,8 @@
 python train_ebm_cifar10_gan_r3gan_ctrl_vit_dec_imagenet.py \
-    --data_path C:/dataset/tiny-imagenet/train/ \
-    --output_dir F:/output/tiny_imagenet_ctrl \
-    --img_size 64 \
-    --patch_size 8 \
+    --data_path /root/autodl-tmp/imagenet100/train/ \
+    --output_dir /root/autodl-tmp/output/imagenet100_ctrl \
+    --img_size 128 \
+    --patch_size 16 \
     --epochs 1200 \
     --batch_size 128 \
     --embed_dim 192 \
